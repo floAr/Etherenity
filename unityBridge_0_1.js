5c68a33fc7b222cbf5002fd0855c233f3c7f112a
Florian Uhde <floar@gmx.de>
2017-06-10 16:53:10 +0200

    Create unityBridge_0_1.js

--- unityBridge_0_1.js
+++ unityBridge_0_1.js
@@ -2,8 +2,9 @@ function getName(){
 console.log('Tried getting name');
     var ABI = [{"constant":false,"inputs":[{"name":"desiredUsername","type":"string"}],"name":"Register","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"_useradress","type":"address"}],"name":"getUsername","outputs":[{"name":"username","type":"string"}],"payable":false,"type":"function"}];
     var contractAddress="0x16d3c3bdf979e08c748c74b84bcc6a4a48d799d5";
-    var contract = web3.eth.contract(contractAddress, ABI);
-contract.call().getUsername(function(error, result){
+    var contract = web3.eth.contract(ABI);
+    var contractInstance=contract.at(contractAddress);
+    contractInstance.getUsername(function(error, result){
      if(!error)
          console.log(result)
      else
