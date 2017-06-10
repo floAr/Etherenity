10f76e2495f88ba007207d4962e33fba97180ae0
Florian Uhde <floar@gmx.de>
2017-06-10 15:35:13 +0200

    trying to hook metamask for testing purpose

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 function onContentLoaded() {
   var accountSpan = document.getElementById("account");
+  if (typeof web3 !== 'undefined') {
+// Use Mist/MetaMask's provider
+window.web3 = new Web3(web3.currentProvider);
+  } else {
+console.log('No web3? You should consider trying MetaMask!')
+// fallback - use your fallback strategy (local node / hosted node + 
+in-dapp id mgmt / fail)
+window.web3 = new Web3(new 
+Web3.providers.HttpProvider("http://localhost:8545"));
+  }
   accountSpan.innerHTML =
 
     (typeof web3 === "undefined" ? "undefined" : web3.eth.accounts[0]);
