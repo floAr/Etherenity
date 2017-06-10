f24b3f1ceb93b4ffaf6bb32ee3b2f45ff0cf9378
Florian Uhde <floar@gmx.de>
2017-06-10 16:21:18 +0200

    Create unityBridge_0_1.js

--- unityBridge_0_1.js
+++ unityBridge_0_1.js
@@ -1,6 +1,9 @@
 function getName(){
 console.log('Tried getting name');
-
+var result = web3.eth.call({
+    to: "0x16d3C3BDF979E08c748C74b84bCc6A4a48d799D5"
+});
+console.log(result); // "0x0000000000000000000000000000000000000000000000000000000000000015"
 }	
 
   function setName( )
