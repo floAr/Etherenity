524889c2583c7d82d3e804b0dc46c067d34515cb
Florian Uhde <floar@gmx.de>
2017-06-10 15:36:59 +0200

    sdsdsdsd

--- app.js
+++ app.js
@@ -1,17 +1,25 @@
 function onContentLoaded() {
   var accountSpan = document.getElementById("account");
-  if (typeof web3 !== 'undefined') {
-// Use Mist/MetaMask's provider
-window.web3 = new Web3(web3.currentProvider);
-  } else {
-console.log('No web3? You should consider trying MetaMask!')
-// fallback - use your fallback strategy (local node / hosted node + 
-in-dapp id mgmt / fail)
-window.web3 = new Web3(new 
-Web3.providers.HttpProvider("http://localhost:8545"));
-  }
+  
   accountSpan.innerHTML =
 
     (typeof web3 === "undefined" ? "undefined" : web3.eth.accounts[0]);
 }
 document.addEventListener("DOMContentLoaded", onContentLoaded);
+
+window.addEventListener('load', function() {
+
+  // Checking if Web3 has been injected by the browser (Mist/MetaMask)
+  if (typeof web3 !== 'undefined') {
+    // Use Mist/MetaMask's provider
+    window.web3 = new Web3(web3.currentProvider);
+  } else {
+    console.log('No web3? You should consider trying MetaMask!')
+    // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
+    window.web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+  }
+
+  // Now you can start your app & access web3 freely:
+  onContentLoaded()
+
+})
