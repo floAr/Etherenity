54510cba7aeba578d35080b6e9443b1b86264c99
Florian Uhde <floar@gmx.de>
2017-06-11 11:46:34 +0200

    sync

--- unityBridge_0_1.js
+++ unityBridge_0_1.js
@@ -5,15 +5,8 @@ console.log('Tried getting name');
     var contractAddress="0x507e049d16c2827ccf603aa2fd882d4a93bec25e";
     var contract = web3.eth.contract(ABI);
     var contractInstance=contract.at(contractAddress);
-    contractInstance.getUsername(function(error, result){
-     if(!error){
-         console.log(result)
-          console.log(error)
-         gameInstance.SendMessage('UIController', 'SetName', result);
-     }
-     else
-         console.error(error);
- });
+    var name=contractInstance.getUsername.call();
+    gameInstance.SendMessage('UIController', 'SetName', name);
 }	
 function setName(name){
    var ABI = [{"constant":false,"inputs":[{"name":"desiredUsername","type":"string"}],"name":"Register","outputs":[{"name":"sender","type":"address"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"_useradress","type":"address"}],"name":"getUsername","outputs":[{"name":"username","type":"string"}],"payable":false,"type":"function"}];
