06bf88e67ed21c29ef9cdb69ba205d7940d8f4f0
Florian Uhde <floar@gmx.de>
2017-06-11 09:54:02 +0200

    Create unityBridge_0_1.js

--- unityBridge_0_1.js
+++ unityBridge_0_1.js
@@ -1,7 +1,8 @@
 function getName(){
 console.log('Tried getting name');
-    var ABI = [{"constant":false,"inputs":[{"name":"desiredUsername","type":"string"}],"name":"Register","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"_useradress","type":"address"}],"name":"getUsername","outputs":[{"name":"username","type":"string"}],"payable":false,"type":"function"}];
-    var contractAddress="0x16d3c3bdf979e08c748c74b84bcc6a4a48d799d5";
+    var ABI = [{"constant":false,"inputs":[{"name":"desiredUsername","type":"string"}],"name":"Register","outputs":[{"name":"sender","type":"address"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"_useradress","type":"address"}],"name":"getUsername","outputs":[{"name":"username","type":"string"}],"payable":false,"type":"function"}]
+;
+    var contractAddress="0x507e049d16c2827ccf603aa2fd882d4a93bec25e";
     var contract = web3.eth.contract(ABI);
     var contractInstance=contract.at(contractAddress);
     contractInstance.getUsername(function(error, result){
@@ -14,8 +15,8 @@ console.log('Tried getting name');
  });
 }	
 function setName(name){
-   var ABI = [{"constant":false,"inputs":[{"name":"desiredUsername","type":"string"}],"name":"Register","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"_useradress","type":"address"}],"name":"getUsername","outputs":[{"name":"username","type":"string"}],"payable":false,"type":"function"}];
-   var contractAddress="0x16d3c3bdf979e08c748c74b84bcc6a4a48d799d5";
+   var ABI = [{"constant":false,"inputs":[{"name":"desiredUsername","type":"string"}],"name":"Register","outputs":[{"name":"sender","type":"address"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"_useradress","type":"address"}],"name":"getUsername","outputs":[{"name":"username","type":"string"}],"payable":false,"type":"function"}];
+   var contractAddress="0x507e049d16c2827ccf603aa2fd882d4a93bec25e";
     var contract = web3.eth.contract(ABI);
     var contractInstance=contract.at(contractAddress);
     contractInstance.Register(name,function(error, result){
